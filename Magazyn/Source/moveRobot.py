c112c581944d18a8a259a244dfc71a7902f270e7
Marek <marekdawidiuk@gmail.com>
2017-04-12 06:35:08 +0200

    Zrobione sterowanie pozycja, dodalem odpowiednie modyfikacje w pliku z vrepem, nie potrzeba juz wielowatkowosci, ale przydalby sie jakis mechanizm sprawdzajacy, czy robot osiagnal juz swoj cel podrozy - nie wazne, czy to obrot, czy jazda do przodu

--- Magazyn/Source/moveRobot.py
+++ Magazyn/Source/moveRobot.py
@@ -1,6 +1,7 @@
-from general import Point
+﻿from general import Point
 import vrep
 import math
+import time
 
 class MoveRobot:
     position = Point()
@@ -9,78 +10,67 @@ class MoveRobot:
     handle = 0
     left_motor_handle = 0
     right_motor_handle = 0
+    R = 0.09717243680870322 #promień koła
+    d = 0.35414159966455727 #odstęp między kołami
 
     def __init__(self, vrepName, clientID):
         self.name = vrepName
         self.clientID = clientID
-        errorCode,self.handle=vrep.simxGetObjectHandle(clientID,name,vrep.simx_opmode_oneshot_wait)
-        errorCode, self.left_motor_handle=vrep.simxGetObjectHandle(clientID,vrepName+'_leftMotor',vrep.simx_opmode_oneshot_wait)#dobrze to robię?
-        errorCode, self.right_motor_handle=vrep.simxGetObjectHandle(clientID,vrepName+'_rightMotor',vrep.simx_opmode_oneshot_wait)#dobrze to robię?
+        errorCode,self.handle=vrep.simxGetObjectHandle(clientID,self.name,vrep.simx_opmode_oneshot_wait)
+        errorCode, self.left_motor_handle=vrep.simxGetObjectHandle(clientID,vrepName+'_leftMotor',vrep.simx_opmode_oneshot_wait)
+        errorCode, self.right_motor_handle=vrep.simxGetObjectHandle(clientID,vrepName+'_rightMotor',vrep.simx_opmode_oneshot_wait)
 
     def goToDestinationPoint(self, destinationPoint): #zakładam, że jestem już obrócony, jeśli nie - dodam kilka linijek
         destX = destinationPoint.x
         destY = destinationPoint.y
         self.updatePosition()
-        vrep.simxSetJointTargetVelocity(self.clientID, self.left_motor_handle, self.speed, vrep.simx_opmode_oneshot_wait)
-        vrep.simxSetJointTargetVelocity(self.clientID, self.right_motor_handle, self.speed, vrep.simx_opmode_oneshot_wait)
-        while math.hypot(self.position.x-destX,self.position.y-destY)>0.1:
-            returnCode, Angles = vrep.simxGetObjectOrientation(self.clientID, self.handle, -1,
-                                                               vrep.simx_opmode_oneshot_wait)
-            self.updatePosition()
-            dx = destX-self.position.x
-            dy = destY-self.position.y
-            angle = math.atan2(dy,dx)
+        dx = destX-self.position.x
+        dy = destY-self.position.y
+        angle = math.atan2(dy,dx)
+        returnCode, Angles = vrep.simxGetObjectOrientation(self.clientID, self.handle, -1,
+                                                           vrep.simx_opmode_oneshot_wait)
+        self.rotate(angle-Angles[2])
+        dist = math.hypot(dx, dy)
 
-            if angle - Angles[2] > 0.2:
-                vrep.simxSetJointTargetVelocity(self.clientID, self.left_motor_handle, 0.8*self.speed, vrep.simx_opmode_oneshot_wait)
-                vrep.simxSetJointTargetVelocity(self.clientID, self.right_motor_handle, 1.2*self.speed, vrep.simx_opmode_oneshot_wait)
 
-            elif angle - Angles[2] > -0.2:
-                vrep.simxSetJointTargetVelocity(self.clientID, self.left_motor_handle, 1.2*self.speed, vrep.simx_opmode_oneshot_wait)
-                vrep.simxSetJointTargetVelocity(self.clientID, self.right_motor_handle, 0.8*self.speed, vrep.simx_opmode_oneshot_wait)
-            else:
-                vrep.simxSetJointTargetVelocity(self.clientID, self.left_motor_handle, self.speed,
-                                                vrep.simx_opmode_oneshot_wait)
-                vrep.simxSetJointTargetVelocity(self.clientID, self.right_motor_handle, self.speed,
-                                                vrep.simx_opmode_oneshot_wait)
+        returnCode, rightAngle = vrep.simxGetJointPosition(self.clientID, self.right_motor_handle, vrep.simx_opmode_oneshot_wait)
+        returnCode, leftAngle = vrep.simxGetJointPosition(self.clientID, self.left_motor_handle, vrep.simx_opmode_oneshot_wait)
 
+
+        rightAngle += dist/self.R
+        leftAngle += dist/self.R
+
+        returnCode = vrep.simxSetJointTargetPosition(self.clientID, self.right_motor_handle, rightAngle, vrep.simx_opmode_oneshot_wait)
+        returnCode = vrep.simxSetJointTargetPosition(self.clientID, self.left_motor_handle, leftAngle, vrep.simx_opmode_oneshot_wait)
+
+    def rotate(self, degrees):
+        #returnCode, Angles = vrep.simxGetObjectOrientation(self.clientID, self.handle, -1, vrep.simx_opmode_oneshot_wait)
+        #ddegree = degrees-Angles[2]
+
+        returnCode, rightAngle = vrep.simxGetJointPosition(self.clientID, self.right_motor_handle, vrep.simx_opmode_oneshot_wait)
+        returnCode, leftAngle = vrep.simxGetJointPosition(self.clientID, self.left_motor_handle, vrep.simx_opmode_oneshot_wait)
+        rightAngle += degrees*self.d/(2*self.R)
+        leftAngle += -degrees*self.d/(2*self.R)
+        returnCode = vrep.simxSetJointTargetPosition(self.clientID, self.right_motor_handle, rightAngle, vrep.simx_opmode_oneshot_wait)
+        returnCode = vrep.simxSetJointTargetPosition(self.clientID, self.left_motor_handle, leftAngle, vrep.simx_opmode_oneshot_wait)
+        time.sleep(2)
 
 
         pass
 
-    def rotate(self, degrees):
-        returnCode, Angles = vrep.simxGetObjectOrientation(self.clientID, self.handle, -1, vrep.simx_opmode_oneshot_wait)
-        ddegree = degrees-Angles[2]
-        if ddegree>math.pi:
-            ddegree = ddegree-(math.ceil(ddegree/math.pi))*math.pi
-        while math.fabs(ddegree)>0.2:
-            #do sprawdzenia - być może roboty będą obracać się w przeciwnym kierunku, niż powinny
-            if math.pi-math.fabs(Angles[2])+math.pi-math.fabs(degrees)<=math.pi:
-                vrep.simxSetJointTargetVelocity(self.clientID, self.left_motor_handle, self.speed, vrep.simx_opmode_oneshot_wait)
-                vrep.simxSetJointTargetVelocity(self.clientID, self.right_motor_handle, -self.speed, vrep.simx_opmode_oneshot_wait)
-            else:
-                vrep.simxSetJointTargetVelocity(self.clientID, self.left_motor_handle, -self.speed, vrep.simx_opmode_oneshot_wait)
-                vrep.simxSetJointTargetVelocity(self.clientID, self.right_motor_handle, self.speed, vrep.simx_opmode_oneshot_wait)
-
-            returnCode, Angles = vrep.simxGetObjectOrientation(self.clientID, self.handle, -1, vrep.simx_opmode_oneshot_wait)
-            ddegree = degrees-Angles[2]
-            if ddegree>math.pi:
-                ddegree = ddegree-(math.ceil(ddegree/math.pi))*math.pi
-
-	pass
-
     def setSpeed(self, velocity):
         self.speed = velocity
-	pass
+        pass
 
     def updatePosition(self):
-        errorCode, PioneerPosition = vrep.simxGetObjectPosition(self.clientID, self.robot_handler, -1, vrep.simx_opmode_oneshot_wait)
+        errorCode, PioneerPosition = vrep.simxGetObjectPosition(self.clientID, self.handle, -1, vrep.simx_opmode_oneshot_wait)
+        print(errorCode, "x", PioneerPosition[0], "y", PioneerPosition[1])
         self.position.x = PioneerPosition[0]
         self.position.y = PioneerPosition[1]
-	pass
+        pass
     
     def getPosition(self):
-	return self.position
+	    return self.position
       
     def getName(self):
-	return self.name
+	    return self.name

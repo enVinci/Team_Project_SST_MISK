c4713fd6f93b3aac1855a8fc4d3ab327b9c349d0
Marek <marekdawidiuk@gmail.com>
2017-05-12 18:57:06 +0200

    Poczatek pracy nad wyznaczaniem trasy

--- Magazyn/Source/moveRobotsInStorehouse.py
+++ Magazyn/Source/moveRobotsInStorehouse.py
@@ -1,13 +1,21 @@
 from .robotInStorehouse import RobotInStorehouse
-from .general import Robots
-from .positions import robotNames
+from .general import *
+from .positions import *
 
 class MoveRobotsInStorehouse:
-	def __init__(self):
-		self.robots = [ RobotInStorehouse(robotNames[i]) for i in range(len(Robots))]
-	
-	def getPalette(self, robotName, paletteId):
-		pass
+    def __init__(self):
+        self.robots = [ RobotInStorehouse(robotNames[i]) for i in range(len(Robots))]
+
+    def getPalette(self, robotName, paletteId):
+        #określenie pozycji palety
+        errorCode, handle = vrep.simxGetObjectHandle(GlobalVar.vrepClientID, palletNames[paletteId], vrepConst.simx_opmode_oneshot_wait)
+        errorCode, Position = vrep.simxGetObjectPosition(GlobalVar.vrepClientID, handle, -1,
+                                                         vrepConst.simx_opmode_oneshot_wait)
+        targetPoint = Point(Position[0], Position[1])
+
+        #jak dojedzie
+        self.robots[int(robotName)].joinPallete(paletteId)
+        pass
 	
 	def putPalette(self, robotName, paletteId):
 		pass
--- Magazyn/Source/robotInStorehouse.py
+++ Magazyn/Source/robotInStorehouse.py
@@ -1,4 +1,8 @@
 from .moveRobot import MoveRobot
+from Source import vrep
+from  Source import vrepConst
+from .globalVar import GlobalVar
+from Source import positions
 
 class RobotInStorehouse:
 	def __init__(self, name):
@@ -12,12 +16,22 @@ class RobotInStorehouse:
 	
 	def goOutOfPath(self, paletteId):
 		pass
-	
+
+	# Według dokumentacji to jest klasa niższego poziomu, niż moveRobotsInStorehouse, więc tutaj wykonuję wywołania
+	# funkcji z v-repa podlączające i odłączające robota do o od palety
+
 	def joinPallete(self, paletteId):
+		errorCode, paletteHandle = vrep.simxGetObjectHandle(GlobalVar.vrepClientID,
+									positions.palletNames(paletteId),
+									vrepConst.simx_opmode_oneshot_wait)
+		vrep.simxSetObjectParent(GlobalVar.vrepClientID, paletteHandle, self.handle, 1, vrep.simx_opmode_oneshot_wait)
 		pass
-	
+
 	def unjoinPalette(self, paletteId):
+		errorCode, paletteHandle = vrep.simxGetObjectHandle(GlobalVar.vrepClientID, positions.palletNames(paletteId),
+															vrepConst.simx_opmode_oneshot_wait)
+		vrep.simxSetObjectParent(GlobalVar.vrepClientID, paletteHandle, -1, 1, vrep.simx_opmode_oneshot_wait)
 		pass
-	
+
 	def setSpeed(self, velocity):
 		self.robot.setSpeed(velocity)

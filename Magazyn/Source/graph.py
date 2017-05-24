599674c10db76119c7e44c27ebeaa0cf40244de3
Marek <marekdawidiuk@gmail.com>
2017-05-24 03:55:48 +0200

    Dziala, mozna testowac, w internecie nie znalazlem gotowca, poprawie moze nazwy zmiennych, zeby bylo czytelniejsze, wyczyszcze z brudow i tyle

--- Magazyn/Source/graph.py
+++ Magazyn/Source/graph.py
@@ -14,8 +14,8 @@ class Graph:
         pass
 
     def addConnection(self, cost, p1, p2):
-        self.nodes.get(p1).update({p2: cost})
-        self.nodes.get(p2).update({p1: cost})
+        self.nodes.get(p1).update({p2: float(cost)})
+        self.nodes.get(p2).update({p1: float(cost)})
         pass
 
     def returnConnections(self, p):
@@ -30,69 +30,120 @@ class Graph:
         for k in self.nodes.keys():
             unvisitedNodes.add(k)
 
-
-        values = [[0 for x in range(len(unvisitedNodes))] for y in range(2)]
+        values = list()
         valuesDict = dict()
+
         i = 0
         for p in unvisitedNodes:
-            values[i][1] = p
             valuesDict.update({p: i})
+            # values[i][0] = p
             if p == pOrigin:
-                values[i][2] = 0
+                values.append([p, 0.0])
+                # values[i][1] = 0.0
             else:
-                values[i][2] = 1e10
+                values.append([p, 1e3])
+                # values[i][1] = 0.0
+            i = i + 1
 
-        i = 0
         while unvisitedNodes != set():
+            print()
+            print()
+            # values = [[0] * len(unvisitedNodes) for i in range(2)]
+            values2 = []
+            valuesDict2 = dict()
+            i = 0
+            for p in unvisitedNodes:
+                valuesDict2.update({p: i})
+                # values[i][0] = p
+                values2.append([p, values[valuesDict.get(p, None)][1]])
+                i = i + 1
+
+            # print("wtf ", values[0][0])
+            print("wypisanie elementow:", values2)
+
             ######Znalezienie najmniejszego elementu
             u = 1e10+1
             k = 0
-            for i in range(len(values)):
-                if u > values[i][2]:
-                    u = values[i][2]
+            for i in range(len(values2)):
+                if u > values2[i][1]:
+                    u = values2[i][1]
                     k = i
+            print()
+            if values2[k][0] == pDest:
+                p1 = pDest
+                path = list()
+                while p1 != pOrigin:
+                    print("Końcowa trasa:", p1)
+                    path.append(p1)
+                    p1 = follower.get(p1)
+                path.append(pOrigin)
+                return path
+            print("najmniejszy element:", k, ", stąd mamy punkt: ", values2[k][0])
+
             #usunięcie elementu o najmniejszej odległości z nieodwiedzonych elementów
-            unvisitedNodes.discard(values[k][1])
+            unvisitedNodes.discard(values2[k][0])
+
+            #uzyskanie listy sąsiadów z nieodwiedzonych elementów
+            a = self.nodes.get(values2[k][0])
 
             subset = set()
-            for k in self.nodes.get(values[k][1], None).keys(): ###Tu skończyłem
-                if k != None:
-                    subset.add(k)
+            for k1 in a.keys():
+                subset.add(k1)
 
             #dla każdego sąsiada wybranego wierzchołka:
             for p in subset:
-                if valuesDict.get(p, None) > values[k][2]+self.nodes.get(p,None).get(values[k][1]):
-                    values[valuesDict.get(p, None)][2] = values[k][2]+self.nodes.get(p,None).get(values[k][1])
-                    follower.update({p: values[k][1]})
-                    unvisitedNodes.add(p)
+                print("Element zbioru: ", p)
+                if valuesDict2.get(p, None) != None:
+                    a1 = values2[valuesDict2.get(p, None)][1]
+                    a2 = values2[k][1]+self.nodes.get(p,None).get(values2[k][0])
+                    print("porównanie", a1, a2)
+                    if a1 > a2:
+                        print("Element zbioru o mniejszej odległości: ", p)
+                        a3 = values2[k][1]
+                        a3 += self.nodes.get(p,None).get(values2[k][0])
+                        # print("test3: ", a3, ", ", type(a3))
+                        print("test1: ", values2[valuesDict2.get(p, None)][1])
+                        values[valuesDict.get(p, None)][1] = a2
+
+                        # print("test test", valuesDict2.get(p, None))
+
+                        values2[valuesDict2.get(p, None)][1] = a2
+                        print("test2: ", values2[valuesDict2.get(p, None)][1])
+                        follower.pop(p, None)
+                        follower.update({p: values2[k][0]})
+                        unvisitedNodes.add(p)
 
         return follower
 
 G = Graph()
 
-G.addNode("x")
-G.addNode("y")
-G.addNode("z")
-G.addNode("a")
-G.addNode("b")
+G.addNode("1")
+G.addNode("2")
+G.addNode("3")
+G.addNode("4")
+G.addNode("5")
+G.addNode("6")
+G.addNode("7")
+G.addNode("8")
 
 
-G.addConnection(1, "x", "a")
+G.addConnection(1, "1", "2")
+G.addConnection(1, "2", "3")
+G.addConnection(1, "3", "8")
+G.addConnection(2, "7", "8")
+G.addConnection(2, "1", "4")
+G.addConnection(2, "4", "5")
+G.addConnection(1, "4", "6")
+G.addConnection(2, "7", "6")
 
-G.addConnection(2, "y", "a")
+# dictionary = G.returnConnections("a")
+# for k,v in dictionary.items():
+#     print(k, v)
+# print("przerwa")
+# dictionary = G.returnConnections("x")
+# for k,v in dictionary.items():
+#     print(k, v)
 
-G.addConnection(3, "z", "a")
-
-G.addConnection(4, "b", "a")
-
-dictionary = G.returnConnections("a")
-for k,v in dictionary.items():
-    print(k, v)
-print("przerwa")
-dictionary = G.returnConnections("x")
-for k,v in dictionary.items():
-    print(k, v)
-
-dictionary = G.findPathDijkstra("x", "y")
-for k, v in dictionary.items():
-    print(k, v)
+Path = G.findPathDijkstra("1", "7")
+for k in Path:
+    print(k)
